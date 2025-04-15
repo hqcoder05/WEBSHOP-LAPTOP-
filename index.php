1ff97b8919a6cafd61fb77ed97cd06fff192eed4
hqcoder05 <hoangquocfw@gmail.com>
2025-04-15 20:36:56 +0700

    index.php

--- index.php
+++ index.php
@@ -1,1 +1,17 @@
 <?php include __DIR__ . '/pages/components/header.php'; ?>
+<?php
+session_start();
+require_once 'includes/autoload.php';
+
+$page = $_GET['page'] ?? 'user/home';
+$pagePath = "pages/$page.php";
+
+if (file_exists($pagePath)) {
+    include "pages/components/header.php";
+    include "pages/components/navbar.php";
+    include $pagePath;
+    include "pages/components/footer.php";
+} else {
+    echo "<h1>404 - Trang không tồn tại</h1>";
+}
+
