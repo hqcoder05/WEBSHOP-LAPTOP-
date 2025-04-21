c2ea1fa5e684704abea99466dbcf657bfafa63ee
TranTuan4610 <165371844+TranTuan4610@users.noreply.github.com>
2025-04-21 14:12:38 +0700

    Update products.php

--- pages/user/products.php
+++ pages/user/products.php
@@ -1,1 +1,61 @@
 <?php
+
+class Product {
+    // Các thuộc tính của sản phẩm
+    private $id;
+    private $name;
+    private $description;
+    private $price;
+    private $quantity;
+
+    // Hàm khởi tạo
+    public function __construct($id, $name, $description, $price, $quantity) {
+        $this->id = $id;
+        $this->name = $name;
+        $this->description = $description;
+        $this->price = $price;
+        $this->quantity = $quantity;
+    }
+
+    // Getter và Setter
+    public function getId() {
+        return $this->id;
+    }
+
+    public function setId($id) {
+        $this->id = $id;
+    }
+
+    public function getName() {
+        return $this->name;
+    }
+
+    public function setName($name) {
+        $this->name = $name;
+    }
+
+    public function getDescription() {
+        return $this->description;
+    }
+
+    public function setDescription($description) {
+        $this->description = $description;
+    }
+
+    public function getPrice() {
+        return $this->price;
+    }
+
+    public function setPrice($price) {
+        $this->price = $price;
+    }
+
+    public function getQuantity() {
+        return $this->quantity;
+    }
+
+    public function setQuantity($quantity) {
+        $this->quantity = $quantity;
+    }
+}
+?>
