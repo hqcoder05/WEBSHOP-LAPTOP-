4f5dbb3e7a4dcf5c5254d7c25431e24bda12bd91
hqcoder05 <ngonhatruong054@gmail.com>
2025-04-30 15:32:33 +0700

    Create admin_header.php

---
+++ pages/admin/admin_header.php
@@ -0,0 +1,11 @@
+<link rel="stylesheet" href="../../assets/css/admin.css">
+<header>
+    <h2>ADMIN - WEBSITE</h2>
+        <nav>
+            <a href="../../pages/admin/dashboard.php">Trang chủ</a> |
+            <a href="../../pages/admin/manage_categories.php">Danh mục</a> |
+            <a href="../../pages/admin/manage_products.php?act=sanpham">Sản phẩm</a> |
+            <a href="../../pages/admin/manage_orders.php?act=tracuudonhang">Đơn hàng</a> 
+        </nav>
+</header>
+        
