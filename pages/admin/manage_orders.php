a840bfbd9c6743a42ae4f6a38608515ea82f8519
hqcoder05 <ngonhatruong054@gmail.com>
2025-04-30 15:32:53 +0700

    Create manage_orders.php

---
+++ pages/admin/manage_orders.php
@@ -0,0 +1,29 @@
+<?php
+  include '../../pages/admin/admin_header.php'
+?>
+  <div class="admin-container">
+    <h1>Quản lý đơn hàng</h1>
+    <table>
+      <thead>
+        <tr>
+          <th>ID Đơn hàng</th>
+          <th>Khách hàng</th>
+          <th>Ngày đặt</th>
+          <th>Tổng tiền</th>
+          <th>Trạng thái</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>001</td>
+          <td>Nguyễn Văn A</td>
+          <td>2025-04-30</td>
+          <td>25.000.000đ</td>
+          <td>Đã giao</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+<?php
+  include '../../pages/admin/admin_footer.php'
+?>
